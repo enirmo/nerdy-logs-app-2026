985fb060475b47f3c40e59b0714f809222516824
Dobrena Pashova <dobrena.pashova@gmail.com>
2026-06-16 00:32:57 +0300

    feat: Added UI and set up home page + auth pages with html and css as a base to test functionality

---
+++ src/main/java/app/web/IndexController.java
@@ -0,0 +1,21 @@
+package app.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+            return "index";
+        }
+
+    @GetMapping("/sign-in")
+    public String getSignInPage() { return "sign_in_pages/sign-in"; }
+
+    @GetMapping("/sign-up")
+    public String getSignUpPage() {
+            return "sign_in_pages/sign-up";
+    }
+}
