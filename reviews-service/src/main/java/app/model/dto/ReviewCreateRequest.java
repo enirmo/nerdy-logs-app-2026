0ef2be998594be50e52db1dc60070eac8ec6b8eb
Dobrena Pashova <dobrena.pashova@gmail.com>
2026-08-08 18:12:29 +0300

    feat: quickfixes on microservice configuration

--- reviews-service/src/main/java/app/model/dto/ReviewCreateRequest.java
+++ reviews-service/src/main/java/app/model/dto/ReviewCreateRequest.java
@@ -3,12 +3,16 @@ package app.model.dto;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 public class ReviewCreateRequest {
